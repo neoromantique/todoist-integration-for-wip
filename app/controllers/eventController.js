3ab3ee0aeed0386bb737e8c20396f1eeecd04e2b
neo <david.aizenberg@paranoici.org>
2018-02-05 02:12:18 +0200

    -debug

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -7,12 +7,11 @@ function eventHandler(req, res) {
     res.sendStatus(200);
   } else if (req.body.event_name == "item:completed") {
     let freq =
-      req.body.initiator.full_name + " Has added a new task called: " +
+      req.body.initiator.full_name + " Has completed the task called: " +
       req.body.event_data.content;
     console.log(freq);
     res.sendStatus(200);
   } else { 
-      console.log(req.body.event_name)
       res.sendStatus(502); 
     }
 }
